d36e686a3e18e9d83b51808520b8f3ea8c49d615
Jeffrey Rios <yefriddavid@gmail.com>
2018-08-31 19:40:13 -0300

    Mejoras

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.send('Hello World ' + process.env.PORT + '!')
 })
 
 
 app.listen(process.env.PORT, () => {
-	console.log('Example app listening on port 3000!')
+	console.log('Example app listening on port ' + process.env.PORT + '!')
 })
 
