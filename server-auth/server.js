420e11d8168045c7c73bc78ce16f6c401b986c71
dave <yefriddavid@gmail.com>
2018-09-12 16:29:30 -0300

    last micro

--- server-auth/server.js
+++ server-auth/server.js
@@ -2,23 +2,35 @@ require('dotenv').config()
 const PORT = process.env.PORT || 81
 const http = require('http')
 const auth = require('basic-auth')
+const proxy = require('http-proxy').createProxyServer()
 
 // Create server
-const server = http.createServer(function (req, res) {
+const server = http.createServer(function (req, res, next) {
   const credentials = auth(req)
-
   if (!credentials || credentials.name !== 'abcd' || credentials.pass !== '1234') {
     res.statusCode = 401
     res.setHeader('WWW-Authenticate', 'Basic realm="example"')
     res.end('Access denied')
   } else {
-    //res.end('Access granted')
-    res.notFound()
+    if(typeof process.env.APP_TARGET == "undefined"){
+      res.end('Access granted')
+    }else{
+      //res.end('else')
+      next()
+      if(process.env.APP_TARGET == ""){
+        //res.end('Access granted')
+        //next()
+        //res.notFound()
+        //res.end(proxy.env.APP_TARGET + ' -> Access granted')
+      }else{
+        /*proxy.web(req, res, {
+          target: process.env.APP_TARGET
+        })*/
+      }
+    }
   }
 })
 
 
-// Listen
 server.listen(PORT)
-
 
